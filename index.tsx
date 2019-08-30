c22a09fe6eed1fa0684f50fb90edae0fe7d2e3b5
Jeff King <jeffking@gmail.com>
2019-08-30 14:50:20 -0700

    Update to new Log[] based SWC.

--- index.tsx
+++ index.tsx
@@ -4,54 +4,44 @@ import {observable} from 'mobx'
 import {observer} from 'mobx-react'
 
 import 'script-loader!vss-web-extension-sdk/lib/VSS.SDK.min.js'
-import {Viewer} from 'sarif-web-component/components/Viewer'
+import {Log, Viewer} from 'sarif-web-component'
 declare var VSS: any
 
 @observer class Tab extends React.Component<any, any> {
 	static decoder = new TextDecoder("utf-8")
-
-	@observable private loading = true
-	@observable.ref private log = undefined
-
+	@observable.ref private logs = undefined as Log[]
 	constructor(props) {
 		super(props)
-		VSS.init({ explicitNotifyLoaded: true, usePlatformScripts: true })
-		// Tfs/WebPlatform/Client/TFS/WorkItemTracking/RestClient.ts
-		VSS.require(['TFS/WorkItemTracking/RestClient'], _WitRestClient => {
+		VSS.init({
+			applyTheme: true,
+			explicitNotifyLoaded: true,
+		})
+		VSS.require(['TFS/WorkItemTracking/RestClient'], witModule => { // Tfs/WebPlatform/Client/TFS/WorkItemTracking/RestClient.ts
 			const onLoaded = async ({id}) => {
-				const witRestClient = _WitRestClient.getClient()
-				const workItem = await witRestClient.getWorkItem(id, null, null, 1)				
+				const witClient = witModule.getClient()
+				const workItem = await witClient.getWorkItem(id, null, null, 1)
 				const files = (workItem.relations || [])
 					.filter(rr => rr.rel === 'AttachedFile' && rr.attributes.name.endsWith(".sarif"))
 					.map((rr, i) => {
 						let p = undefined
 						return {
 							key: i, text: rr.attributes.name,
-							sarif: async () => p = p || (async () =>
-								Tab.decoder.decode(await witRestClient.getAttachmentContent(rr.url.substr(-36), rr.attributes.name))
-							)()
+							sarif: async () => p = p || Tab.decoder.decode(await witClient.getAttachmentContent(rr.url.substr(-36), rr.attributes.name))
 						}
 					})
 
-				const first = files[0]
-				if (first) {
-					const foo = await first.sarif() as string
-					this.log = JSON.parse(foo)
-					console.log(this.log)
-				}
-				this.loading = false
+				const logTexts = await Promise.all(files.map(async file => await file.sarif())) as string[]
+				const logs = logTexts.map(log => JSON.parse(log) as Log)
+				this.logs = logs
 			}
 			VSS.register(VSS.getContribution().id, { onLoaded }) // ;onLoaded({ id: 1 })
-			VSS.notifyLoadSucceeded()
+			VSS.notifyLoadSucceeded() // Not working within onLoaded()
 		})
 	}
 	render() {
-		const {loading, log} = this
-		// const dd = <Dropdown className="resultsDropdown"
-		// 	options={files} selectedKey={fileIndex}
-		// 	onChange={(ev, option, i) => this.setState({ fileIndex: i })} />
-		return loading || log
-			? <Viewer log={log} />
+		const {logs} = this
+		return !logs || logs.length
+			? <Viewer logs={logs} />
 			: <div className="full">No SARIF attachments found.</div>
 	}
 }
