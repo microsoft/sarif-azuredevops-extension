9bc233386dd930d62040a2fafa29000c1492e086
Jeff King <jeffking@gmail.com>
2020-09-24 17:13:39 -0700

    Refactor webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 const path = require('path')
+
+const common = {
+	mode: 'production',
+	resolve: {
+		// LHS must match webpack `externals` of sarif-web-component.
+		extensions: ['.js', '.ts', '.tsx'],
+		alias: {
+			'React': path.resolve('node_modules/react'),
+			'ReactDOM': path.resolve('node_modules/react-dom'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{ test: /\.png$/, use: 'url-loader' },
+			{ test: /\.woff$/, use: 'url-loader' },
+		]
+	},
+}
 
 module.exports = [
 	{
+		...common,
 		entry: path.join(__dirname, 'src', 'build.tsx'),
 		output: { path: path.join(__dirname, 'src'), filename: 'build.js' },
-		mode: 'production',
-		resolve: {
-			extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			alias: {
-				// LHS must match webpack `externals` of sarif-web-component.
-				'React': path.resolve('node_modules/react'),
-				'ReactDOM': path.resolve('node_modules/react-dom'),
-			},
-		},
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/
-				},
-				{
-					test: /\.s?css$/,
-					use: ['style-loader', 'css-loader', 'sass-loader']
-				},
-				{ test: /\.png$/, use: 'url-loader' },
-				{ test: /\.woff$/, use: 'url-loader' },
-			]
-		},
 		performance: {
 			maxAssetSize: 1.3 * 1024 * 1024,
 			maxEntrypointSize: 1.3 * 1024 * 1024,
@@ -37,39 +44,15 @@ module.exports = [
 			port: 8080,
 			stats: 'none',
 			https: true,
-		}
+		},
 	},	
 	{
+		...common,
 		entry: path.join(__dirname, 'src', 'workItem.tsx'),
 		output: { path: path.join(__dirname, 'src'), filename: 'workItem.js' },
-		mode: 'production',
-		resolve: {
-			extensions: ['.js', '.ts', '.tsx'],
-			alias: {
-				'React': path.resolve('node_modules/react'),
-				'ReactDOM': path.resolve('node_modules/react-dom'),
-			},
-		},
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/
-				},
-				{
-					test: /\.s?css$/,
-					use: ['style-loader', 'css-loader', 'sass-loader']
-				},
-				{ test: /\.png$/, use: 'url-loader' },
-				{ test: /\.woff$/, use: 'url-loader' },
-			]
-		},
 		performance: {
 			maxAssetSize: 1.12 * 1024 * 1024,
 			maxEntrypointSize: 1.12 * 1024 * 1024,
 		},
-		stats: 'minimal',
-		devServer : { port: 8080, https: true, stats: 'none' }
 	},
 ]
