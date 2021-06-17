a0ad06c0a2e3955c67e7e5ffa5b929e9758d4ab8
Jeff King <jeffking@gmail.com>
2021-06-16 18:31:19 -0700

    Accomodate OneBranch Artifact names.

--- src/build.getArtifactsFileEntries.test.ts
+++ src/build.getArtifactsFileEntries.test.ts
@@ -4,5 +4,5 @@ import { buildClient } from './mockBuildClient'
 test('Artifacts are filtered correctly by name.', async () => {
 	const files = await getArtifactsFileEntries(buildClient, '', 0)
 	const logTexts = await Promise.all(files.map(async file => await file.contentsPromise))
-	expect(logTexts).toHaveLength(2)
+	expect(logTexts).toHaveLength(4)
 })
--- src/build.getArtifactsFileEntries.ts
+++ src/build.getArtifactsFileEntries.ts
@@ -13,16 +13,23 @@ export async function getArtifactsFileEntries(
 	): Promise<FileEntry[]> {
 
 	const artifacts = await buildClient.getArtifacts(project, buildId)
-	const files = await (async () => {
-		if (!artifacts.some(a => a.name === 'CodeAnalysisLogs')) return []
-		const arrayBuffer = await buildClient.getArtifactContentZip(project, buildId, 'CodeAnalysisLogs')
-		const zip = await JSZip.loadAsync(arrayBuffer)
-		return Object.values(zip.files)
-			.filter(entry => !entry.dir && entry.name.endsWith('.sarif'))
-			.map(entry => ({
-				name:            entry.name.replace('CodeAnalysisLogs/', ''),
-				contentsPromise: entry.async('string')
-			}))
-	})()
-	return files
+	const files = await Promise.all(
+		artifacts
+			.filter(artifact => {
+				return artifact.name === 'CodeAnalysisLogs'
+					|| artifact.name.includes('_sdl_analysis') // OneBranch
+					|| artifact.name.endsWith('_sdl_sources')  // OneBranch
+			})
+			.map(async artifact => {
+				const arrayBuffer = await buildClient.getArtifactContentZip(project, buildId, artifact.name)
+				const zip = await JSZip.loadAsync(arrayBuffer)
+				return Object.values(zip.files)
+					.filter(entry => !entry.dir && entry.name.endsWith('.sarif'))
+					.map(entry => ({
+						name:            entry.name.replace(`${artifact.name}/`, ''),
+						contentsPromise: entry.async('string')
+					}))
+			})
+	)
+	return files.flat()
 }
