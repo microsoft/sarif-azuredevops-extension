cd6dae57b08eca1b1e2783d33d373cc28f1669d1
Chris Meyer <cmeyer@microsoft.com>
2023-04-10 12:30:11 -1000

    Catch exception when request for artifact zip file fails

--- src/build.getArtifactsFileEntries.ts
+++ src/build.getArtifactsFileEntries.ts
@@ -1,5 +1,6 @@
 import * as JSZip from 'jszip'
 import { ArtifactBuildRestClient } from './ArtifactBuildRestClient'
+import { AppInsights } from 'applicationinsights-js'
 
 interface FileEntry {
 	name: string,
@@ -24,18 +25,23 @@ export async function getArtifactsFileEntries(
 					|| artifact.name.endsWith('_sdl_sources')  // OneBranch
 			})
 			.map(async artifact => {
-				const arrayBuffer = await buildClient.getArtifactContentZip(project, buildId, artifact.name)
-				const zip = await JSZip.loadAsync(arrayBuffer)
-				return Object
-				    .values(zip.files)
-					.filter(entry => !entry.dir && entry.name.endsWith('.sarif'))
-					.map(entry => ({
-						name:            entry.name.replace(`${artifact.name}/`, ''),
-						artifactName:    artifact.name,
-						filePath:        entry.name.replace(`${artifact.name}/`, ''),
-						buildId:         buildId,
-						contentsPromise: entry.async('string')
-					}))
+				try {
+					const arrayBuffer = await buildClient.getArtifactContentZip(project, buildId, artifact.name)
+					const zip = await JSZip.loadAsync(arrayBuffer)
+					return Object
+						.values(zip.files)
+						.filter(entry => !entry.dir && entry.name.endsWith('.sarif'))
+						.map(entry => ({
+							name:            entry.name.replace(`${artifact.name}/`, ''),
+							artifactName:    artifact.name,
+							filePath:        entry.name.replace(`${artifact.name}/`, ''),
+							buildId:         buildId,
+							contentsPromise: entry.async('string')
+						}))
+				} catch (e) {
+					AppInsights.trackException(e, null, { artifactName: artifact.name, buildId: `${buildId}` })
+					return [];
+				}
 			})
 	)
 	return files.flat()
